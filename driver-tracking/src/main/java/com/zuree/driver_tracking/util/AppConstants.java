3017beffa9e79e10c07f080be1b2f611857215b4
Akhand <akhandsingh1010@gmail.com>
2025-05-24 23:23:10 +0530

    Remove extra constant

--- driver-tracking/src/main/java/com/zuree/driver_tracking/util/AppConstants.java
+++ driver-tracking/src/main/java/com/zuree/driver_tracking/util/AppConstants.java
@@ -5,5 +5,4 @@ import java.time.format.DateTimeFormatter;
 public class AppConstants {
     public static final DateTimeFormatter CUSTOM_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
     public static final String ZONE_ID = "Asia/Kolkata";
-    public static final String API_VERSION = "v1";
 }
