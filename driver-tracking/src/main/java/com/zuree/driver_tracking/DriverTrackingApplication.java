532c654c859cbf07802fa7470e95d6ddc3dc923f
Akhand <akhandsingh1010@gmail.com>
2025-05-24 23:15:57 +0530

    Test and main class added

---
+++ driver-tracking/src/main/java/com/zuree/driver_tracking/DriverTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.zuree.driver_tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriverTrackingApplication.class, args);
+	}
+
+}
