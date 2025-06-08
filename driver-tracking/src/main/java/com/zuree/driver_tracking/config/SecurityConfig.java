fa99e4f6018b08ef0bcda5f827a059715191b484
Akhand <akhandsingh1010@gmail.com>
2025-06-08 22:27:34 +0530

    Success and Security Config

--- driver-tracking/src/main/java/com/zuree/driver_tracking/config/SecurityConfig.java
+++ driver-tracking/src/main/java/com/zuree/driver_tracking/config/SecurityConfig.java
@@ -1,24 +1,62 @@
 package com.zuree.driver_tracking.config;
 
-
+import com.zuree.driver_tracking.util.AppConstants;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
+import javax.crypto.spec.SecretKeySpec;
+
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
+    @Value("${api.version}")
+    private String apiVersion;
+
+    @Value("${api.base-version}")
+    private String apiBaseVersion;
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
+                        .requestMatchers("/auth/" + apiVersion + "/**", "/auth/" + apiBaseVersion + "/**",
+                                "/alarm/" + apiBaseVersion + "/**", "/alarm/" + apiVersion + "/create",
+                                "/swagger-ui/**", "/v3/api-docs/**", "/actuator/**",
+                                "/swagger-ui.html").permitAll()
+                        .anyRequest().authenticated()
                 )
-                .csrf(AbstractHttpConfigurer::disable); // ✅ Modern way to disable CSRF
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt
+                                .jwtAuthenticationConverter(jwtAuthenticationConverter())
+                        )
+                );
 
         return http.build();
     }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withSecretKey(new SecretKeySpec(
+                jwtSecret.getBytes(),
+                AppConstants.JWT_ALGORITHM)).build();
+    }
+
+    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        return new JwtAuthenticationConverter();
+    }
 }
+
 
